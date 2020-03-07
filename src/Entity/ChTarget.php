73d931fc32a59d582fa046cd8e6295474da7a366
notcgi <2ravno2@gmail.com>
2020-03-07 10:04:23 +0300

    .

--- src/Entity/ChTarget.php
+++ src/Entity/ChTarget.php
@@ -172,7 +172,7 @@ class ChTarget
 
     public function getLink($last=true)
     {
-        echo "totime ". $this->totime."<br>";
+        echo "totime ". $this->totime->format('Y-m-d H:i:s')."<br>";
         return '/children/' . $this->getChild() . '#' . (($last and  (( $this->collected < $this->goal ) or (date($this->totime) > date( "now"))))  ? 'needed' : 'kid-news');
     }
 
