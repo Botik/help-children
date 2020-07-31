8b02a77cede76eb95bb064ac844b2daf4ac99b7c
moseffect21 <moseffect21@gmail.com>
2020-07-31 16:15:25 +0300

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
 		var $page = $('html, body');
 		if($(this).hasClass('dolg-opeka')) {
 			$page.animate({
-				scrollTop: ($('.dolg-opeka-item').offset().top - 80
+				scrollTop: ($('.dolg-opeka-item').offset().top - 80)
 			}, 400);
 		} else {
 			$page.animate({
-				scrollTop: ($('.gift-section').offset().top - 80
+				scrollTop: ($('.gift-section').offset().top - 80)
 			}, 400);
 		}
 	});
