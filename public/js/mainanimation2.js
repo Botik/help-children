8bb454cea1f567552bf58436a180b3dc77ed8303
moseffect21 <moseffect21@gmail.com>
2020-08-18 11:19:24 +0300

    test

--- public/js/mainanimation2.js
+++ public/js/mainanimation2.js
@@ -279,7 +279,7 @@ $(document).ready(function(){
         .to('.fixed-animation-block .right-half .empty-cart.with-cont .reabilitation-block .reabGone',{opacity:1,display:'flex',duration:0.5},'stage4+=0.5')
 
         .to('.fixed-animation-block .list_item:nth-child(4) .subinfo',{opacity:1,duration:0.5},'stage4+=1.5')
-        .to('.fixed-animation-block .list_item:nth-child(4) .subinfo2',{duration:1.5})
+        .to('.fixed-animation-block .list_item:nth-child(4) .subinfo2',{duration:2.5})
 
 // stage4 +=2
         .to('.fixed-animation-block .list_item:nth-child(4) .subinfo',{opacity:0,duration:1},'stage4+=4')
